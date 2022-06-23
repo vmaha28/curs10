80cf45028ccdbdcaa79f1d73190a84fba3a63a66
Vlad <vlad.mahalean@gmail.com>
2022-06-23 20:49:47 +0300

    hei

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args, String salut) {
         System.out.println("Hello World!");
         System.out.println(salut);
+        System.out.println("Hei");
     }
 }
